ff7d16aad9c3065f64154cb4a103bfe4c5ff11ee
Yagyamitra-Gade <yagyamitrag@gmail.com>
2026-09-04 19:05:38 +0530

    Update assignment_1.cpp

--- assignment_1.cpp
+++ assignment_1.cpp
@@ -1,1 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class book_details
+{
+  public:
+  int bookID;
+  string name;
+  string publisher;
+  float price;
+
+  void display_information()
+  {
+     cout<<" #### BOOK_DETAILS #### "<<endl;
+     cout<<"Book Id is : "<<bookID<<endl;
+     cout<<"Book name is : "<<name<<endl;
+     cout<<"Book Publisher :
+"<<publisher<<endl;
+     cout<<"Book Price is : "<<price<<endl;
+  }
+};
+
 int main()
+{
+    book_details b1;
+    b1.bookID=98;
+    b1.name="coumpter Science";
+    b1.publisher="Arihant";
+    b1.price=450.60;
+    b1.display_information();
+
+    return 0;
+}

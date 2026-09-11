6a3cbe279d07a76b08b6110feddecece0d83a539
Yagyamitra-Gade <yagyamitrag@gmail.com>
2026-09-11 12:21:40 +0530

    Update assignment_5.cpp

--- assignment_5.cpp
+++ assignment_5.cpp
@@ -9,21 +9,23 @@ public:
     float percentage;
     int marks;
 
+    // Constructor defined inside class body
+    student(string name, int rollno, float percentage, int marks) {
+        this->name = name;
+        this->rollno = rollno;
+        this->percentage = percentage;
+        this->marks = marks;
+    }
+
     void display()
-    {
-        cout<<"#### STUDENT DETAILS ####"<<endl;
-        cout << "NAME OF STUDENT IS :" <<this-> name << endl;
+    { 
+        cout<<"#### STUDENT DETAILS  #### " <<endl;        cout << "NAME OF STUDENT IS :" << this->name << endl;
         cout << "ROLL.NO OF STUDENT IS : " << this->rollno << endl;
         cout << "PERCENTAGE OF STUDENT IS : " << this->percentage << endl;
         cout << "MARKS OF STUDENT IS : " << this->marks << endl;
     }
 };
-    student(string name, int rollno,float percentage,  int marks) {
-    this->name = name;
-    this->rollno = rollno;
-    this->percentage = percentage;
-    this->marks = marks;
-}
+
 int main() {
     student s1("JAY",56,86.5,450);
     s1.display();
